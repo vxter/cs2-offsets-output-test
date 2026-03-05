18e00e167e7f3b971676e5989f92cc0168eae593
Vxter <jasotufy@gmail.com>
2026-03-05 14:08:20 +1300

    Update offsets.

--- networksystem_dll.rs
+++ networksystem_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-26 01:09:29.816350400 UTC
+// 2026-03-05 01:06:18.170845 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
--- worldrenderer_dll.cs
+++ worldrenderer_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-26 01:09:29.816350400 UTC
+// 2026-03-05 01:06:18.170845 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: worldrenderer.dll
